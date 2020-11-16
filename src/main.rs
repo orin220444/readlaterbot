61f232b8d51cda03960dbfbec98fe121b939004f
Orin Serimon <orin220444@protonmail.com>
2020-11-16 13:31:46 +0700

    WIP: simple ref + handling commands(not working yet)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate dotenv;
 use dotenv::dotenv;
-use teloxide::prelude::*;
+use teloxide::{prelude::*, utils::command::BotCommand};
 mod link_finder;
 mod post;
 use post::post::Post;
@@ -9,13 +9,24 @@ async fn main() {
     dotenv().ok();
     run_bot().await;
 }
-
+#[derive(BotCommand)]
+enum Command {
+    Random,
+}
+async fn command_answer(cx: UpdateWithCx<Message>, command: Command) -> ResponseResult<()>{
+match command {
+    Command::Random => { cx.answer("test").send().await;
+Ok(())
+    }
+}
+}
 async fn run_bot() {
     teloxide::enable_logging!();
     log::info!("Starting dices_bot...");
 
     let bot = Bot::from_env();
-
+/*let cloned_bot = bot.clone();
+    teloxide::commands_repl(cloned_bot, "test_bot_name", command_answer).await;
     teloxide::repl(bot, |message| async move {
         println!("{:#?}", &message.update.kind);
         let urls = link_finder::link_finder::link_finder(&message);
@@ -34,11 +45,15 @@ async fn run_bot() {
         }
             None => println!("No urls!"),
 
-
         }
 
         message.answer_dice().send().await?;
         ResponseResult::<()>::Ok(())
     })
     .await;
+  */
+    Dispatcher::new(bot).messages_handler(|rx: DispatcherHandlerRx<Message>|{
+    rx.for_each_concurrent(None, |message| async move {
+        println!("{:#?}", message);
+    })});
 }
