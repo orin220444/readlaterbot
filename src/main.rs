57d8c25d8f275ec5e8a3c96edf28be705010067a
Orin Serimon <orin220444@protonmail.com>
2020-11-18 10:30:19 +0700

    code formatting

--- src/main.rs
+++ src/main.rs
@@ -14,49 +14,45 @@ async fn main() {
 enum Command {
     Random,
 }
-async fn command_answer(cx: UpdateWithCx<Message>, command: Command) -> ResponseResult<()>{
-match command {
-    Command::Random => { cx.answer("test").send().await?;
-        ResponseResult::<()>::Ok(())
+async fn command_answer(cx: UpdateWithCx<Message>, command: Command) -> ResponseResult<()> {
+    match command {
+        Command::Random => {
+            cx.answer("test").send().await?;
+            ResponseResult::<()>::Ok(())
+        }
     }
 }
-}
-async fn handle_message(cx: UpdateWithCx<Message>) -> ResponseResult<()>{
-    match cx.update.text(){
-    None => {
-ResponseResult::<()>::Ok(())
-    }
-    Some(text) => {
-    if let Ok(command) = Command::parse(text, "test_name_bot") {
-        command_answer(cx, command).await?;
-        ResponseResult::<()>::Ok(())
-    }
-    else {
-        println!("{:#?}", &cx.update.kind);
-        let urls = link_finder::link_finder::link_finder(&cx);
-        match urls {
-            None => println!("No urls!"),
-            Some(urls) => {
-                println!("{:#?}", urls);
-                for url in urls {
-                    let mut post = Post::new(url);
-                    post.real_url().await;
-                    match post.save_post().await {
-                        Ok(_) => log::info!("Successful saved post"),
-                        Err(e) => println!("{:#?}", e),
+async fn handle_message(cx: UpdateWithCx<Message>) -> ResponseResult<()> {
+    match cx.update.text() {
+        None => ResponseResult::<()>::Ok(()),
+        Some(text) => {
+            if let Ok(command) = Command::parse(text, "test_name_bot") {
+                command_answer(cx, command).await?;
+                ResponseResult::<()>::Ok(())
+            } else {
+                println!("{:#?}", &cx.update.kind);
+                let urls = link_finder::link_finder::link_finder(&cx);
+                match urls {
+                    None => println!("No urls!"),
+                    Some(urls) => {
+                        println!("{:#?}", urls);
+                        for url in urls {
+                            let mut post = Post::new(url);
+                            post.real_url().await;
+                            match post.save_post().await {
+                                Ok(_) => log::info!("Successful saved post"),
+                                Err(e) => println!("{:#?}", e),
+                            }
+                        }
                     }
+                }
 
+                cx.answer_dice().send().await?;
+                ResponseResult::<()>::Ok(())
             }
         }
-
-        }
-
-        cx.answer_dice().send().await?;
-        ResponseResult::<()>::Ok(())
     }
-    }
-    }
-    }
+}
 async fn run_bot() {
     teloxide::enable_logging!();
     log::info!("Starting dices_bot...");
@@ -65,16 +61,14 @@ async fn run_bot() {
     //.await;
 
     Dispatcher::new(bot)
-        .messages_handler(|rx: DispatcherHandlerRx<Message>|{
-    rx.for_each_concurrent(None, |cx| async move {
-        match handle_message(cx).await {
-Ok(_) => {}
-Err(e) => {println!("Error while handling messages: {:#?}", e)
-
-}
-        };
-    })
+        .messages_handler(|rx: DispatcherHandlerRx<Message>| {
+            rx.for_each_concurrent(None, |cx| async move {
+                match handle_message(cx).await {
+                    Ok(_) => {}
+                    Err(e) => println!("Error while handling messages: {:#?}", e),
+                };
+            })
         })
-
-    .dispatch().await;
+        .dispatch()
+        .await;
 }
--- src/post.rs
+++ src/post.rs
@@ -1,5 +1,5 @@
 pub mod post {
-            use rusqlite::{params, Connection, Result};
+    use rusqlite::{params, Connection, Result};
     pub struct Post {
         pub original_url: String,
         pub real_url: Option<String>,
@@ -11,60 +11,57 @@ pub mod post {
             Post {
                 original_url,
                 real_url: None,
-                read: false
+                read: false,
             }
         }
 
-        pub async fn save_post(self, ) -> Result<()> {
-                        self.save_to_db().await?;
+        pub async fn save_post(self) -> Result<()> {
+            self.save_to_db().await?;
             Ok(())
         }
 
-        async fn save_to_db(self) -> Result<()>{
+        async fn save_to_db(self) -> Result<()> {
             let path = "./readlaterdb.db3";
             let conn = Connection::open(&path)?;
-                conn.execute(
-                    "CREATE TABLE IF NOT EXISTS post (
+            conn.execute(
+                "CREATE TABLE IF NOT EXISTS post (
                         original_url    TEXT PRIMARY KEY,
                         real_url        TEXT
                     )",
-                    params![],
-                )?;
-            conn.execute(
-
-                "INSERT INTO post (original_url, real_url) VALUES (?1,?2)"
-            , params![
-            self.original_url, 
-            self.real_url
-            ],
+                params![],
             )?;
-Ok(())
+            conn.execute(
+                "INSERT INTO post (original_url, real_url) VALUES (?1,?2)",
+                params![self.original_url, self.real_url],
+            )?;
+            Ok(())
         }
-pub async fn real_url(& mut self) -> & Post {
-    match reqwest::get(&self.original_url.to_string()).await {
-        Err(e) => {println!("{:#?}", e); self}
-        Ok(res) => {
-            //let body = &res.text().await?;
-            println!("{:#?}", &res);
-            let real_url = {
-                let mut host: String = String::new();
-                match res.url().host() {
-                    Some(host_path) => host = host_path.to_string(),
-                    None => {
-                        println!("No host!");
-                    }
+        pub async fn real_url(&mut self) -> &Post {
+            match reqwest::get(&self.original_url.to_string()).await {
+                Err(e) => {
+                    println!("{:#?}", e);
+                    self
                 }
-                let path = res.url().path().to_string();
-                host + &path
-            };
-            println!("{}", real_url);
+                Ok(res) => {
+                    //let body = &res.text().await?;
+                    println!("{:#?}", &res);
+                    let real_url = {
+                        let mut host: String = String::new();
+                        match res.url().host() {
+                            Some(host_path) => host = host_path.to_string(),
+                            None => {
+                                println!("No host!");
+                            }
+                        }
+                        let path = res.url().path().to_string();
+                        host + &path
+                    };
+                    println!("{}", real_url);
 
-self.real_url = Some(real_url);
-    self
+                    self.real_url = Some(real_url);
+                    self
+                }
+            }
         }
-    }
-
-
-}
     }
 }
