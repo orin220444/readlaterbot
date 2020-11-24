acd3a502028ccf4b048ae82de176ad1410f89acf
Orin Serimon <orin220444@protonmail.com>
2020-11-24 13:31:39 +0700

    add navigate buttons(not working)

--- src/handlers/get_all_posts.rs
+++ src/handlers/get_all_posts.rs
@@ -5,13 +5,31 @@ use teloxide::{
 pub async fn get_all_posts(cx: UpdateWithCx<Message>) {
     match crate::post::Post::get_all_posts().await {
         Ok(posts) => {
-            let mut button_posts = Vec::with_capacity(posts.len());
-            for post in posts {
+            let stop:usize = {
+                if posts.len() >= 50 {
+                    49
+                }
+                else {
+                    posts.len()
+                }
+            };
+            let mut button_posts = Vec::with_capacity(stop + 1);
+            for post in &posts[0..stop] {
                 button_posts.push(vec![InlineKeyboardButton::new(
                     &post.original_url,
                     InlineKeyboardButtonKind::CallbackData(String::from(&post.original_url)),
                 )]);
             }
+            button_posts.push(vec![
+                InlineKeyboardButton::new(
+                    "<-",
+                    InlineKeyboardButtonKind::CallbackData("prev page".to_string()),
+                ),
+                InlineKeyboardButton::new(
+                    "->",
+                    InlineKeyboardButtonKind::CallbackData("next page".to_string()),
+                ),
+            ]);
             println!("{:#?}", button_posts);
             match cx
                 .answer("All Posts".to_string())
