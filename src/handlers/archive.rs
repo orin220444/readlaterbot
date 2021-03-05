4db8bd7c042de513d4d9abd3bd93439b8e07c959
Orin Serimon <orin220444@protonmail.com>
2021-03-05 19:22:20 +0700

    now archived posts marks [Archived]

--- src/handlers/archive.rs
+++ src/handlers/archive.rs
@@ -2,7 +2,7 @@ use crate::Post;
 use anyhow::Result;
 use teloxide::{
     prelude::{Request, UpdateWithCx},
-    types::{CallbackQuery, MediaKind, MessageKind},
+    types::{CallbackQuery, ChatId, ChatOrInlineMessage, MediaKind, MessageKind},
 };
 pub async fn archive(cx: UpdateWithCx<CallbackQuery>) -> Result<()> {
     let url: Option<&String> = {
@@ -21,9 +21,21 @@ pub async fn archive(cx: UpdateWithCx<CallbackQuery>) -> Result<()> {
         None => Ok(()),
         Some(url) => {
             Post::archive_post(url).await?;
+            let message_id = cx.update.message.clone().unwrap().id;
+            let chat_id = cx.update.message.clone().unwrap().chat_id();
             cx.bot
                 .answer_callback_query(cx.update.id)
                 .text("Archived!")
+                .send()
+                .await?;
+            cx.bot
+                .edit_message_text(
+                    ChatOrInlineMessage::Chat {
+                        chat_id: ChatId::Id(chat_id),
+                        message_id: message_id,
+                    },
+                    format!("{} [Archived]", cx.update.message.unwrap().text().unwrap()),
+                )
                 .send()
                 .await?;
             Ok(())
