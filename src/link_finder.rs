f3f3efc801be38a3691e2924200a4a0101ea1115
Orin Serimon <orin220444@protonmail.com>
2021-03-07 18:59:21 +0700

    remove some debug prints

--- src/link_finder.rs
+++ src/link_finder.rs
@@ -4,37 +4,29 @@ use teloxide::types::{MediaKind, Message, MessageEntityKind, MessageKind};
 pub fn link_finder(message: &UpdateWithCx<Message>) -> Option<Vec<String>> {
     let message_kind = &message.update.kind;
     match message_kind {
-        MessageKind::Common(message_common) => {
-            println!("{:#?}", message_common.media_kind);
-            match &message_common.media_kind {
-                MediaKind::Text(text_data) => {
-                    let mut urls: Vec<String> = Vec::new();
-                    println!("{:#?}", &text_data);
-                    for entity in &text_data.entities {
-                        println!("{:#?}", entity);
-                        match &entity.kind {
-                            MessageEntityKind::TextLink { url } => urls.push(url.to_string()),
-                            MessageEntityKind::Url => {
-                                println!("{:#?}", &text_data);
-
-                                let url: String = {
-                                    let text = &text_data.text;
-                                    let start = entity.offset;
-                                    let length = entity.length;
-                                    text.chars().skip(start).take(length).collect()
-                                };
-                                println!("{:#?}", url);
-                                urls.push(url.to_string());
-                                println!("{:#?}", &entity)
-                            }
-                            _ => println!("No needed entities!"),
-                        };
-                    }
-                    Some(urls)
+        MessageKind::Common(message_common) => match &message_common.media_kind {
+            MediaKind::Text(text_data) => {
+                let mut urls: Vec<String> = Vec::new();
+                for entity in &text_data.entities {
+                    match &entity.kind {
+                        MessageEntityKind::TextLink { url } => urls.push(url.to_string()),
+                        MessageEntityKind::Url => {
+                            let url: String = {
+                                let text = &text_data.text;
+                                let start = entity.offset;
+                                let length = entity.length;
+                                text.chars().skip(start).take(length).collect()
+                            };
+                            println!("{:#?}", url);
+                            urls.push(url.to_string());
+                        }
+                        _ => println!("No needed entities!"),
+                    };
                 }
-                _ => None,
+                Some(urls)
             }
-        }
+            _ => None,
+        },
         _ => None,
     }
 }
