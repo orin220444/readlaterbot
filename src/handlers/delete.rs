d4c4589b24e46d26b82f5a96255f67af52de469e
Orin Serimon <orin220444@protonmail.com>
2021-04-01 22:16:38 +0700

    because autosend feature enabled, delete .send()

--- src/handlers/delete.rs
+++ src/handlers/delete.rs
@@ -9,13 +9,11 @@ pub async fn delete(cx: UpdateWithCx<AutoSend<Bot>, CallbackQuery>, data: &str) 
     cx.requester
         .answer_callback_query(cx.update.id)
         .text("Deleted!")
-        .send()
         .await?;
     if let Some(message) = cx.update.message {
         println!("test");
         cx.requester
             .delete_message(message.chat.id, message.id)
-            .send()
             .await?;
     }
     Ok(())
